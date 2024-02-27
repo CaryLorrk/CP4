02a6ca086c0673e4a9548c5cbaf120baef447380
CaryLorrk <carylorrk@gmail.com>
2024-02-27 16:14:43 +0800

    g.5.Kattis_creditcard

---
+++ Ch1/1.6/g.5.Kattis_creditcard/run.py
@@ -0,0 +1,42 @@
+import sys
+
+
+def main():
+    lines = sys.stdin.read().splitlines()
+    n_cases = int(lines[0])
+    lines = lines[1:]
+
+    for n in range(n_cases):
+        r, b, m = lines[n].split()
+        r = float(r)/100
+        b = int(float(b)*1000)
+        m = int(float(m)*1000)
+        p = 0
+        while b >= 5:
+            i = b * r
+            if i % 10 >= 5:
+                i -= i % 10 
+                i += 10
+            else:
+                i -= i % 10
+            p += 1
+            b += i
+            if b % 10 >= 5:
+                b -= b % 10 
+                b += 10
+            else:
+                b -= b % 10
+
+            b = b - m
+            if p > 1200:
+                break
+
+        if p > 1200:
+            print('impossible')
+        else:
+            print(p)
+
+    
+
+if __name__ == "__main__":
+    main()
